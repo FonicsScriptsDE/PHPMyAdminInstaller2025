47bee199de8a9c47e9d2bfeed02e7f4643009fe1
FonicsScriptsDE <166014883+FonicsScriptsDE@users.noreply.github.com>
2024-11-14 11:18:21 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#    Copyright (C) 2021  Julian G.
+#    Copyright (C) 2025  Jens FonicsScriptsDE.
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License 2 as published by
